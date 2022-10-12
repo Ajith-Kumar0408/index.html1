05a3e8caf439a1d99176129dd718a120764f90a1
Ajith-Kumar0408 <ajithak0093@gmail.com>
2022-10-12 13:20:32 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function foo(){
+    var res=document.getElementById("firstname").value;
+    var res1=document.getElementById("middlename").value;
+    var res2=document.getElementById("lastname").value;
+    var res3=document.getElementById("mail").value;
+    var res4=document.getElementById("password").value;
+    console.log(res,res1,res2,res3,res4);
+
+}
