f272bcd2733717c00eccd6e0f1f316a1587ebf87
Ajith-Kumar0408 <ajithak0093@gmail.com>
2022-10-16 00:00:03 +0530

    index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-function foo(){
-    var res=document.getElementById("firstname").value;
-    var res1=document.getElementById("middlename").value;
-    var res2=document.getElementById("lastname").value;
-    var res3=document.getElementById("mail").value;
-    var res4=document.getElementById("password").value;
-    console.log(res,res1,res2,res3,res4);
-
-}
